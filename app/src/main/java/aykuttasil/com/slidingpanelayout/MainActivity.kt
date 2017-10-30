b55b4a3dd5c5930ffabcb50dc588a046e0ac40a0
Aykut Asil <aykuttasil@gmail.com>
2017-10-30 23:44:02 +0300

    initial commit

---
+++ app/src/main/java/aykuttasil/com/slidingpanelayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package aykuttasil.com.slidingpanelayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sliding_pane_layout)
+    }
+}
